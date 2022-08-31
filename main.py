746f18067b177918694e3a37f899d1a70e474d7d
ochi <ocos54ce@gmail.com>
2022-08-31 19:11:08 +0900

    to create new file

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# これはサンプルの Python スクリプトです。
+import json
+import os
+import re
+import sys
 
-# ⌃R を押して実行するか、ご自身のコードに置き換えてください。
-# ⇧ を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
+import requests
+
+GRAPHQL_URL = "https://leetcode.com/graphql"
+query = """\
+query questionData($titleSlug: String!) {
+    question(titleSlug: $titleSlug) {
+        questionId
+        title
+        translatedTitle
+        codeDefinition
+        content
+        translatedContent
+        difficulty
+        likes
+        dislikes
+        similarQuestions
+        topicTags {
+            name
+            slug
+            translatedName
+            __typename
+        }
+        codeSnippets {
+            lang
+            langSlug
+            code
+            __typename
+        }
+        stats
+        hints
+        exampleTestcases
+        sampleTestCase
+        metaData
+        enableRunCode
+    }
+}
+"""
 
 
-def print_hi(name):
-    # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
-    print(f'Hi, {name}')  # ⌘F8を押すとブレークポイントを切り替えます。
+def getQuestionTitle(url):
+    pattern = re.compile('(?<=https://leetcode.com/problems/)[^/]*', )
+    title = re.search(pattern, url)
+    return title.group()
 
 
-# ガター内の緑色のボタンを押すとスクリプトを実行します。
+def fetchQuestion(title):
+    variables = {'titleSlug': title}
+    r = requests.get(GRAPHQL_URL, json={'query': query, 'variables': variables})
+    return r.json()
+
+
+def getTargetTemplate(data):
+    template_format = json.loads(data['data']['question']['codeDefinition'])
+    filtered_data = list(filter(lambda lang: lang['value'] == 'python3', template_format))
+    if len(filtered_data) != 1:
+        raise ValueError("取得したデータが正しくありません")
+    return filtered_data[0]['defaultCode']
+
+
+def createSolutionFile(dirname, code):
+    if not (os.path.exists(dirname)):
+        os.mkdir(dirname)
+    try:
+        with open(f'{dirname}/solution.py', "x") as f:
+            f.write(code)
+            print('\033[32m' + f'{dirname}/solution.pyを作成しました。' + '\033[0m')
+    except FileExistsError:
+        print('\033[31m' + 'ファイルが既に存在します。' + '\033[0m')
+
+
+def init():
+    args = sys.argv
+    title = getQuestionTitle(args[1])
+    data = fetchQuestion(title)
+    python_template = getTargetTemplate(data)
+    createSolutionFile(title, python_template)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
+    init()
