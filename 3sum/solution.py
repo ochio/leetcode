09fbb9c951781821d47530c5204ad5870122243c
ochi <ocos54ce@gmail.com>
2023-02-03 13:39:47 +0900

    modifiy 3sum

--- 3sum/solution.py
+++ 3sum/solution.py
@@ -9,10 +9,7 @@ class Solution:
             left = i + 1
             right = len(nums) - 1
 
-            while True:
-                if left == right:
-                    break
-
+            while left < right:
                 n = nums[i] + nums[left] + nums[right]
 
                 if n < 0:
@@ -21,5 +18,11 @@ class Solution:
                     right -= 1
                 else:
                     l.append([nums[i], nums[left], nums[right]])
-                    break
+                    left += 1
+                    right -= 1
+
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+                    while nums[right] == nums[right + 1] and left < right:
+                        right -= 1
         return l
