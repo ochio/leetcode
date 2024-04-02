178933909469c24d000c553b867328145f1e8728
ochi <ocos54ce@gmail.com>
2024-04-02 22:59:50 +0900

    concatenation-of-array

---
+++ concatenation-of-array/solution/src/main.rs
@@ -0,0 +1,12 @@
+impl Solution {
+  pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+    let mut ans:Vec<i32> = Vec::new();
+    for n in &nums {
+      ans.push(*n);
+    }
+    for n in &nums {
+      ans.push(*n);
+    }
+    ans
+  }
+}
